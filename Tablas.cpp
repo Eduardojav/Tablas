2b059ff22606ec209b0385a43ec835f977d3e776
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2017-09-12 05:54:25 -0500

    Add files via upload

---
+++ Tablas.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int A=0;
+int B=0;
+int C=0;
+int D=0;
+int a;
+
+int solo(int a,int b){
+    if ((a==1 && b==1)||(a==0 && b==0)){
+        return 0;}
+    return 1;}
+int yyyy (int a,int b){
+    if ((a==1 && b==0)||(a==0 && b ==0)){
+        return 0;}
+    return 1;}
+int ooo(int a,int b){
+    if (a==0 && b ==0){
+        return 0;}
+    return 1;}
+void recorer(m){
+    for(i=0,i<)}
+int main(){
+cout<<"ingresa el tamaño de tu problema logico"<<endl;
+cin>>a;
+int m[a];
+cin>>m[a];
+cout<<m[a]<<endl;
+return 0;}
+
