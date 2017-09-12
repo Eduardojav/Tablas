d83d188058fb1b460a87967ca228cee7a71b7255
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2017-09-12 06:43:17 -0500

    Update Tablas.cpp

--- Tablas.cpp
+++ Tablas.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int A=0;
+int A=0;//definimos las variables con valor 0 para hacerlos como booleanos
 int B=0;
 int C=0;
 int D=0;
@@ -18,7 +18,7 @@ int ooo(int a,int b){
     if (a==0 && b ==0){
         return 0;}
     return 1;}
-void tabla(int& A, int& B,int& C,int& D){
+void tabla(int& A, int& B,int& C,int& D){// aqui haremos las 16 combinaciones que se deben hacer con 4 variables con sus valores de 0 y 1
     for(int i=0;i<2;i++){
             if(i==0){
                 A=0;}
@@ -40,18 +40,18 @@ void tabla(int& A, int& B,int& C,int& D){
                             else {
                             D=1;}
                         void recorer(int,int);
-                        cout<<"|"<<A<<"|"<<B<<"|"<<C<<"|"<<D<<"|"<<endl;
+                        cout<<"|"<<A<<"|"<<B<<"|"<<C<<"|"<<D<<"|"<<endl;//imrpimiremos siempre la tabla con los valores de A,B,C,D
     }
     }
 
     }}}
 int main(){
-cout<<"ingresa el tama�o de tu problema logico"<<endl;
+cout<<"ingresa el tamaño de tu problema logico"<<endl;//el tamaño del problema 
 cin>>a;
-int m[a];
+int m[a];//creamos una lista que le daremos valores enteros
 cout<<"ingresa tu problema logico"<<endl;
-cin>>m[a];
-void recorer(int m[] ,int a){
+cin>>m[a];//le daremos valores a la lista
+void recorer(int m[] ,int a){// aqui recorremos la lista en busca de los conectores logicos para rezlizar las operaciones
     for(int i=0;i<a;i++){
         if (m[a]=="&"){
             m[a-1] * m[a+1];}
